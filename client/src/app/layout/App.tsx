6964d55582460422b7ae69a6cc801f7d0d9a8ec3
Adam M <whiskypo082@gmail.com>
2024-12-03 22:28:35 -0600

    End of section 3

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,39 @@
+
+import Catalog from "../../features/catalog/Catalog";
+import Header from "./Header";
+import React, { useState } from "react";
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+function App() {  
+
+  const [darkMode, setDarkMode] = useState(false); 
+  const paletteType = darkMode ? 'dark' : 'light'; 
+  const theme = createTheme({
+    palette: { 
+      mode: paletteType, 
+      background: { 
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+
+  function handleThemeChange() { 
+    setDarkMode(!darkMode); 
+  }
+
+  return (
+    <React.Fragment>   
+      <ThemeProvider theme={theme}>
+
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+
+      <Container>
+        <Catalog />         
+      </Container>
+      </ThemeProvider>
+    </React.Fragment>
+  )
+}
+
+export default App
